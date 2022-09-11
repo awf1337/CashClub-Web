93c5ccbad3809f8b60928578ff4817fe363784fe
awf1337 <101428860+awf1337@users.noreply.github.com>
2022-09-12 02:55:57 +0300

    API calls on coupons page

--- src/Components/CouponsCardSort.js
+++ src/Components/CouponsCardSort.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React,{useState} from "react";
 
-export default function CouponsCard({couponsName}) {
+export default function CouponsCard({couponsName, endPointvalue, endPointId}) {
+    const [disabled, setDisabled] = useState(false)
+
     const optionsComponent = couponsName.map((e,i) => {
         return (
-            <option key={i}>{e}</option>
+            <option key={i} value={e.id}>{e.name}</option>
         )
     })
 
@@ -11,18 +13,23 @@ export default function CouponsCard({couponsName}) {
         <div>
             <div className="sort-group">
                 <div className="filter-default">
-                    <select className="filter" id="filter-coupons">
+                    <select 
+                    className="filter" 
+                    id="filter-coupons" 
+                    onChange={(e) => endPointId(e.target.value)}
+                    >
                         <option value="all">Toate cupoanele</option>
                         { optionsComponent }
                     </select>
                 </div>
 
                 <div className="filter-default">
-                    <select className="filter" id="sort-coupons">
-                        <option value="default">Sorteaza</option>
-                        <option>Cele mai populare</option>
-                        <option>Expira in curand</option>
-                        <option>Valuarea reducerii</option>
+                    <select className="filter" id="sort-coupons" onChange={(e) => {endPointvalue(e);setDisabled(true)}}  >
+                        <option value="" disabled={disabled} >Sorteaza</option>
+                        <option value="">Populare</option>
+                        <option value="&sortby=start_date&sortdir=desc">Cele mai noi</option>
+                        <option value="&filters[expireSoon]=1&sortby=end_date&sortdir=asc">Expira in curand</option>
+                        <option value="&sortby=discount_percent&sortdir=desc">Valuarea reducerii</option>
                     </select>
                 </div>
             </div>
--- src/Pages/DiscountCoupons.js
+++ src/Pages/DiscountCoupons.js
@@ -1,20 +1,22 @@
 import React, {useState,useEffect} from "react";
-import CouponsCard from "../Components/CouponsCardSort";
+import CouponsCardSort from "../Components/CouponsCardSort";
 import CouponsCardDisplay from "../Components/CouponsCardDisplay";
 
 export default function DiscountCoupons() {
-    const [coupons,setCoupons] = useState([])
-    const [shopsName,setShopsName] = useState([])
+    const [coupons, setCoupons] = useState([])
+    const [concatCoupons, setConcatCoupons] = useState([])
+    const [shopsName, setShopsName] = useState([])
+    const [disableButton, setDisableButton] = useState(false)
 
     async function getShopData() {
         const res = await fetch("https://api.cashclub.ro/api/coupons/shops");
         const data = await res.json();
 
-        setShopsName(data.map((e) => e.name))
+        setShopsName(data)
     }
 
-    async function getCouponsData() {
-        const res = await fetch("https://api.cashclub.ro/api/coupons?page=1&perPage=40");
+    async function getCouponsData(apiEndPoint) {
+        const res = await fetch(`https://api.cashclub.ro/api/coupons?page=1&perPage=40${apiEndPoint}`);
         const data = await res.json();
 
         setCoupons(data.data);
@@ -22,10 +24,28 @@ export default function DiscountCoupons() {
 
     useEffect(() => {
         getShopData();
-        getCouponsData();
-    },[])
+        getCouponsData("");
+    },[]);
 
-    console.log(coupons)
+    async function setCouponsDisplay(apiEndPoint) {
+        const res = await fetch(`https://api.cashclub.ro/api/coupons?${apiEndPoint}`);
+        const data = await res.json();
+
+        setConcatCoupons(data.data)
+    }
+
+    useEffect(()=> {
+        setCoupons(coupons.concat(concatCoupons))
+    },[concatCoupons]);
+
+    function setExtendedCoupons() {
+        setCouponsDisplay("pageNumber=2&perPage=40");
+        setDisableButton(!disableButton);
+    }
+
+    function sortSpecial(e) {
+        getCouponsData(e)
+    }
 
     const couponsDisplayElement = coupons.map((element,i) => {
         return (
@@ -43,12 +63,23 @@ export default function DiscountCoupons() {
             <p>Coduri de reducere exclusive la magazinele 
             partenere. Acestea se aplica pe cosul de cumparaturi inainte de finalizarea comenzii.</p>
             
-            <CouponsCard 
+            <CouponsCardSort 
             couponsName = {shopsName}
+            endPointvalue = {(e) => sortSpecial(e.target.value)}
+            endPointId = {(e) => {sortSpecial(`&filters[shopId]=${e}`); setDisableButton(true)}}
             />
+
             <div className="couponsWrapper">
                 <div className="couponsList">
+
                     {couponsDisplayElement}
+
+                    <button 
+                    className="generalButtons read-moreBTN"
+                    onClick={()=> setExtendedCoupons()}
+                    disabled= {disableButton}
+                    style = {!disableButton ? {opacity:1} : {opacity:0.3}}
+                    >Mai multe cupoane</button>
                 </div>
             </div>
         </div>
